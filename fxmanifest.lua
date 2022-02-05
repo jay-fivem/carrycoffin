86b97f74ca9a5e44a62600c69686e44ab108a5a4
Jay <80186604+jay-fivem@users.noreply.github.com>
2022-02-05 12:20:36 +1030

    uplaoded wrong one

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,10 +1,4 @@
 fx_version 'cerulean'
 game 'gta5'
 
-author 'Mystic'
-description 'mystic-vehiclemenu'
-
-client_scripts {
-    '@menuv/menuv.lua',
-    'client.lua'
-}
+client_script 'client.lua'
